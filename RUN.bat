c5cf2acbf21a2c94a289b825400a8762d93ed136
Daryl Daruwala <55801366+Vohala@users.noreply.github.com>
2025-09-28 17:44:37 +0530

    Update RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -1,5 +1,4 @@
 @echo off
-:: Elevate to Administrator if not already
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo Requesting administrative privileges...
@@ -7,10 +6,9 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-:: Set PowerShell execution policy for CurrentUser
 powershell -NoProfile -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force"
 
-:: Run the Install.ps1 script
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Install.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Browser_Install.ps1"
+
 
 pause
